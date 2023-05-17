7db104d1963a8011080473723e82419c1817011e
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-05-17 21:29:57 +0530

    Create First_N_Natural_Rev_Num.java

---
+++ First_N_Natural_Rev_Num.java
@@ -0,0 +1,16 @@
+public class First_N_Natural_Rev_Num {
+		
+		static void printN(int n) {
+			
+			if(n == 0) return ;
+			
+			System.out.println(n);
+			printN(n-1);
+		}
+
+		public static void main(String[] args) {
+			printN(5);
+	}
+}
+
+
